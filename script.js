e2b55e09b8a2d0c39551e6bad99c3b068d724c2f
squach28 <squach3443@gmail.com>
2024-11-13 17:35:57 -0800

    add footer with practice details + socials

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const registerMenuListener = () => {
   const menuButton = document.getElementById("menu-icon-button");
   const mobileMenu = document.getElementById("mobile-menu");
   menuButton.addEventListener("click", () => {
-    mobileMenu.classList.toggle("block");
+    mobileMenu.classList.toggle("viewable");
   });
 };
 
