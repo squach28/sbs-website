6439a0ecd9da97f48c4b93279d27554e48a19c89
squach28 <squach3443@gmail.com>
2025-02-21 12:22:22 -0800

    change hero image on main page to banner, let list elements in mobile menu display after a delay

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
 const registerMenuListener = () => {
   const menuButton = document.getElementById("menu-icon-button");
   const mobileMenu = document.getElementById("mobile-menu");
+  const mobileMenuElements = mobileMenu.querySelectorAll("li");
+  console.log(mobileMenuElements);
   menuButton.addEventListener("click", () => {
     mobileMenu.style.width = "100%";
+    mobileMenuElements.forEach((listElement) => {
+      setTimeout(() => {
+        listElement.style.display = "block";
+      }, 250);
+    });
   });
 };
 
@@ -11,8 +18,12 @@ const registerMobileMenuCloseButton = () => {
     "mobile-menu-close-btn"
   );
   const mobileMenu = document.getElementById("mobile-menu");
+  const mobileMenuElements = mobileMenu.querySelectorAll("li");
   mobileMenuCloseButton.addEventListener("click", () => {
     mobileMenu.style.width = "0";
+    mobileMenuElements.forEach((listElement) => {
+      listElement.style.display = "none";
+    });
   });
 };
 
